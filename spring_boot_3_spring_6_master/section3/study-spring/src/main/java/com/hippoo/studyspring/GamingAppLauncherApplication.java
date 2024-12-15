1d73d94406ee74e424b2b6b983baa7c986013e5c
qyinm <qusseun@gmail.com>
2024-12-15 20:58:32 +0900

    refactoring by using Spring Component

---
+++ spring_boot_3_spring_6_master/section3/study-spring/src/main/java/com/hippoo/studyspring/GamingAppLauncherApplication.java
@@ -0,0 +1,22 @@
+package com.hippoo.studyspring;
+
+import com.hippoo.studyspring.game.GameRunner;
+import com.hippoo.studyspring.game.GamingConsole;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.hippoo.studyspring.game")
+public class GamingAppLauncherApplication {
+
+    public static void main(String[] args) {
+        try (var context = new AnnotationConfigApplicationContext(
+                GamingAppLauncherApplication.class)) {
+
+            context.getBean(GamingConsole.class).up();
+            context.getBean(GameRunner.class).run();
+        }
+
+    }
+}
---
+++ spring_boot_3_spring_6_master/section3/study-spring/src/main/java/com/hippoo/studyspring/game/SuperContraGame.java
@@ -0,0 +1,24 @@
+package com.hippoo.studyspring.game;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
+@Qualifier("SuperContraGameQualifier")
+public class SuperContraGame implements GamingConsole {
+    public void up() {
+        System.out.println("up");
+    }
+
+    public void down() {
+        System.out.println("sit down");
+    }
+
+    public void left() {
+        System.out.println("Go back");
+    }
+
+    public void right() {
+        System.out.println("shoot a bullet");
+    }
+}
