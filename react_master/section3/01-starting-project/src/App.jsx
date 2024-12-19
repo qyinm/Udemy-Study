4fcd6767f5aa5d777706978502b7afd6f6210371
qyinm <qusseun@gmail.com>
2024-12-19 11:00:36 +0900

    react section 3 finish

--- react_master/section3/01-starting-project/src/App.jsx
+++ react_master/section3/01-starting-project/src/App.jsx
@@ -1,32 +1,75 @@
-import reactImg from './assets/react-core-concepts.png';
-
-const reactDescriptions = ['Fundamental', 'Crucial', 'Core']
-
-function getRandomInt(max) {
-  return Math.floor(Math.random() * (max + 1));
-}
-
-function Header() {
-  const description = reactDescriptions[getRandomInt(2)];
-
-  return (
-    <header>
-      <img src={reactImg} alt="Stylized atom" />
-      <h1>React Essentials</h1>
-      <p>
-        {description} React concepts you will need for almost any app you are
-        going to build!
-      </p>
-    </header>
-  );
-}
+import { CORE_CONCEPTS, EXAMPLES } from "./data.js";
+import Header from "./components/Header/Header.jsx";
+import CoreConcept from "./components/CoreConcept.jsx";
+import TabButton from "./components/TabButton.jsx";
+import { useState } from "react";
 
 function App() {
+  const [selectedTopic, setSelectedTopic] = useState();
+  let tabContent = <p>Please select a topic.</p>;
+  if (selectedTopic) {
+    tabContent = (
+      <div id="tab-content">
+        <h3>{EXAMPLES[selectedTopic].title}</h3>
+        <p>{EXAMPLES[selectedTopic].description}</p>
+        <pre>
+          <code>{EXAMPLES[selectedTopic].code}</code>
+        </pre>
+      </div>
+    );
+  }
+  function handleSelect(selectedButton) {
+    // console.log(selectedButton);
+    setSelectedTopic(selectedButton);
+  }
   return (
     <div>
+      <Header />
       <main>
-        <Header />
-        <h2>Time to get started!</h2>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            {CORE_CONCEPTS.map((conceptItem) => (
+              <CoreConcept
+                key={conceptItem.title}
+                // title={core.title}
+                // description={core.description}
+                // image={core.image}
+                {...conceptItem}
+              />
+            ))}
+          </ul>
+        </section>
+        <section id="examples">
+          <h2>Example</h2>
+          <menu>
+            <TabButton
+              isSelected={selectedTopic === "components"}
+              onSelect={() => handleSelect("components")}
+            >
+              Components
+            </TabButton>
+            <TabButton
+              isSelected={selectedTopic === "jsx"}
+              onSelect={() => handleSelect("jsx")}
+            >
+              JSX
+            </TabButton>
+            <TabButton
+              isSelected={selectedTopic === "props"}
+              onSelect={() => handleSelect("props")}
+            >
+              Props
+            </TabButton>
+            <TabButton
+              isSelected={selectedTopic === "state"}
+              onSelect={() => handleSelect("state")}
+            >
+              State
+            </TabButton>
+          </menu>
+          {tabContent}
+        </section>
       </main>
     </div>
   );
---
+++ react_master/section3/01-starting-project/src/components/TabButton.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function TabButton({children, onSelect, isSelected}) {
+
+  return (
+    <li>
+      <button className={isSelected ? "active" : undefined} onClick={onSelect}>{children}</button>
+    </li>
+  );
+}
