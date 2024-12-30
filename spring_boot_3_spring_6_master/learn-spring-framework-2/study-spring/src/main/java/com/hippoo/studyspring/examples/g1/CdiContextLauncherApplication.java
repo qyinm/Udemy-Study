37d37ed6e658d61faf303bb632684e84dffc1d4b
qyinm <qusseun@gmail.com>
2024-12-30 17:28:10 +0900

    jakarta EE CDI exercise

---
+++ spring_boot_3_spring_6_master/learn-spring-framework-2/study-spring/src/main/java/com/hippoo/studyspring/examples/g1/CdiContextLauncherApplication.java
@@ -0,0 +1,49 @@
+package com.hippoo.studyspring.examples.g1;
+
+import jakarta.inject.Inject;
+import jakarta.inject.Named;
+import java.util.Arrays;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+//@Component
+@Named
+class BusinessService {
+    DataService dataService;
+
+    public DataService getDataService() {
+        return dataService;
+    }
+
+    //@Autowired
+    @Inject
+    public void setDataService(DataService dataService) {
+        System.out.println("setter Injection!");
+        this.dataService = dataService;
+    }
+}
+
+//@Component
+@Named
+class DataService {
+
+}
+
+@Configuration
+@ComponentScan
+public class CdiContextLauncherApplication {
+
+    public static void main(String[] args) {
+        try (var context = new AnnotationConfigApplicationContext(
+                CdiContextLauncherApplication.class)) {
+
+            Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
+
+            System.out.println(context.getBean(BusinessService.class).getDataService());
+        }
+
+    }
+}
