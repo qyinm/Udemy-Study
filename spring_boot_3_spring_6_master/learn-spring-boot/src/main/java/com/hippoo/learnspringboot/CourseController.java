145089857b2ec45227f5a5a362261adf53b070cd
qyinm <qusseun@gmail.com>
2024-12-30 23:13:58 +0900

    first RESTful API

---
+++ spring_boot_3_spring_6_master/learn-spring-boot/src/main/java/com/hippoo/learnspringboot/CourseController.java
@@ -0,0 +1,17 @@
+package com.hippoo.learnspringboot;
+
+import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CourseController {
+
+    @GetMapping("/courses")
+    public List<Course> retrieveAllCourses() {
+        return List.of(
+                new Course(1, "Learn AWS", "inflearn"),
+                new Course(2, "Learn DevOps", "udemy")
+        );
+    }
+}
