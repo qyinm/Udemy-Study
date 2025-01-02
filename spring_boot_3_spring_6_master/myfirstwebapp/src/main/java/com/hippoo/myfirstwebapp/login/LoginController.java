b150063e6e37033f04658774766d6a46d7f7cdfb
qyinm <qusseun@gmail.com>
2025-01-02 20:32:13 +0900

    jsp request param, using Model and ModelMap

---
+++ spring_boot_3_spring_6_master/myfirstwebapp/src/main/java/com/hippoo/myfirstwebapp/login/LoginController.java
@@ -0,0 +1,20 @@
+package com.hippoo.myfirstwebapp.login;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class LoginController {
+
+    // Model
+    @RequestMapping("/login")
+//    public String gotoLoginPage(@RequestParam String name, Model model) {
+    public String gotoLoginPage(@RequestParam String name, ModelMap model) {
+//            model.addAttribute("name",name);
+        model.put("name", name);
+        return"login";
+}
+}
---
+++ spring_boot_3_spring_6_master/myfirstwebapp/src/main/resources/META-INF/resources/WEB-INF/jsp/login.jsp
@@ -0,0 +1,13 @@
+<html>
+<head>
+    <title>Login Page</title>
+</head>
+<body>
+<h1>Welcome to the login page!</h1>
+<h1>name ${name}</h1>
+<form action="/login" method="post">
+    <input type="text" name="username"/>
+    <input type="password" name="password"/>
+</form>
+</body>
+</html>
